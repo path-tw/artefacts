46b9782b2a92d04b28f53b6b4b247cde62070ff7
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2024-06-24 02:53:51 +0530

    Update the link to latest release

--- setup.ps1
+++ setup.ps1
@@ -8,6 +8,6 @@ if ([System.IntPtr]::Size -eq 4) {
 mkdir -p "$location" -ErrorAction SilentlyContinue
 cd "$location"
 
-Invoke-WebRequest "https://github.com/path-tw/artefacts/releases/download/v0.1.1/devsetup-v0.1.1-windows-$arch.exe" -OutFile devsetup
+Invoke-WebRequest "https://github.com/path-tw/dev-setup/releases/download/v0.1.2/devsetup-v0.1.2-windows-$arch.exe" -OutFile devsetup
 
 Start-Process ".\devsetup" -ArgumentList "setup" -Verb RunAs -Wait
