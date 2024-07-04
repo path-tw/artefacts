61d25b05dfb93eab4079f60d0eb6e9b9727dab77
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2024-07-04 11:03:36 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -8,6 +8,6 @@ if ([System.IntPtr]::Size -eq 4) {
 mkdir -p "$location" -ErrorAction SilentlyContinue
 cd "$location"
 
-Invoke-WebRequest "https://github.com/path-tw/artefacts/releases/download/v0.1.4/devsetup-v0.1.4-windows-$arch.exe" -OutFile argus.exe
+Invoke-WebRequest "https://github.com/path-tw/artefacts/releases/download/v0.1.5/devsetup-v0.1.5-windows-$arch.exe" -OutFile argus.exe
 
 Start-Process ".\argus.exe" -ArgumentList "setup" -Wait
