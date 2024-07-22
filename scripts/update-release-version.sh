e376c035cc9cd9a432717f1dc838256fce7134f0
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2024-07-23 01:12:48 +0530

    change directory before making any changes

--- scripts/update-release-version.sh
+++ scripts/update-release-version.sh
@@ -48,4 +48,7 @@ function main {
   git push origin main
 }
 
-main
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
+pushd "${PROJECT_DIR}" > /dev/null
+  main
+popd > /dev/null
