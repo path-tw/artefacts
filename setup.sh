704cf52bb0e9f27f5774fa1162f7397598f4edd5
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2024-07-23 00:51:52 +0530

    testing

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ cd "$location"
 apt update -y
 apt install wget curl -y
 
-curl -Ls "https://github.com/path-tw/artefacts/releases/download/v0.1.6/devsetup-v0.1.6-linux-$arch" -o argus
+curl -Ls "https://github.com/path-tw/artefacts/releases/download/v0.1.6-TESTING/devsetup-v0.1.6-TESTING-linux-$arch" -o argus
 
 chmod +x argus
 ./argus setup
